bacc257b579ccc4c886dddaf03f10f83237ddc91
noninus <rafinha.nonino7@gmail.com>
2021-01-14 11:50:21 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,95 @@
 import Head from 'next/head'
+import React from 'react';
 import styles from '../styles/Home.module.css'
+import Grid from '@material-ui/core/Grid';
+import { useRouter } from 'next/router'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 export default function Home() {
+  const [opt1, setOpt1] = React.useState('');
+  const [opt2, setOpt2] = React.useState('');
+  const [answer, setAnswer] = React.useState(null);
+
+  const handleClick =() => {
+    let min = Math.ceil(0);
+    let max = Math.floor(2);
+    let random = Math.floor(Math.random() * (max - min)) + min;
+    setAnswer(random === 0? opt1 : opt2);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Me ajuda decidir</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Me ajuda decidir
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Digite uma pergunta que o polvo decide pra ti!
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+        {answer ?  <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center" 
+          spacing={3}>
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+              spacing={3} >
+              <TextField disabled={true} id="filled-basic" label="Opção 01" variant="filled" onChange={(e) => setOpt1(e.target.value)} />
+              <Grid item> <p>vs</p>  </Grid>
+              <TextField disabled={true} id="filled-basic" label="Opção 02" variant="filled" onChange={(e) => setOpt2(e.target.value)} />
+            </Grid>
+            <Grid item>
+              <Button variant="contained" onClick={() => setAnswer(null)}>Tentar novamente</Button>
+            </Grid>
+            <p style={{visibility: answer === null ? 'hidden': ''}}className={styles.subtitle}>
+              {answer === null ? 'a' : answer}
             </p>
-          </a>
-        </div>
+        </Grid>:  <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center" 
+          spacing={3}>
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+              spacing={3} >
+              <TextField id="filled-basic" label="Opção 01" variant="filled" onChange={(e) => setOpt1(e.target.value)} />
+              <Grid item> <p>vs</p>  </Grid>
+              <TextField id="filled-basic" label="Opção 02" variant="filled" onChange={(e) => setOpt2(e.target.value)} />
+            </Grid>
+            <Grid item>
+              <Button disabled={opt1 === '' || opt2 === '' ? true : false}variant="contained" onClick={handleClick}>Decidir</Button>
+            </Grid>
+            <p style={{visibility: answer === null ? 'hidden': ''}}className={styles.subtitle}>
+              {'hidden'}
+            </p>
+        </Grid>}
+        
+
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/Noninus"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Desenvolvido por Rafael Nonino Filho
+        
         </a>
       </footer>
     </div>
